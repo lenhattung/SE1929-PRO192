e42ce2600411a393a5cbfd6f53fe26023fd9d264
Tung Le <lenhattung@gmail.com>
2025-01-14 15:29:27 +0700

    update

---
+++ JavaApplication_03_a/src/MainClass3.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tungi
+ */
+public class MainClass3 {
+    public static void main(String[] args){
+        double a = Double.parseDouble(args[0]);
+        double b = Double.parseDouble(args[1]);
+        System.out.println(a+b);
+    }
+}
