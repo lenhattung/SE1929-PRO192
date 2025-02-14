b4c0a0a956f75a12d02f029b35e2ea00166ca931
Tung Le <lenhattung@gmail.com>
2025-02-14 15:56:42 +0700

    ud
    UD

--- ASM/ASM02/Q2/src/src/Audi.java
+++ ASM/ASM02/Q2/src/src/Audi.java
@@ -8,6 +8,64 @@
  *
  * @author tungi
  */
-public class Audi extends Vehicle{
-    
+public class Audi extends Vehicle {
+
+    private int releasedYear;
+    private String color;
+
+    public Audi() {
+        super("", "", 0, 0);
+        setReleasedYear(0);
+        setColor("");
+    }
+
+    public Audi(String id, String name, double price, int quantity, String color, int releasedYear) {
+        super(id, name, price, quantity);
+        setReleasedYear(releasedYear);
+        setColor(color);
+    }
+
+    public int getReleasedYear() {
+        return releasedYear;
+    }
+
+    public void setReleasedYear(int releasedYear) {
+        if (releasedYear < 2022 || releasedYear > 2025) {
+            this.releasedYear = 2022;
+        }
+        this.releasedYear = releasedYear;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        if (color.equals("Black") || color.equals("White") || color.equals("Red") || color.equals("Gray")) {
+            this.color = color;
+        } else {
+            this.color = "Black";
+        }
+    }
+
+    @Override
+    public String toString() {
+        return getId()+","+
+                getName()+","+
+                getColor()+","+
+                getReleasedYear()+","+
+                String.format("%.2f", getPrice())+","+
+                getQuantity()+","+
+                String.format("%.2f", getSubTotal());
+    }
+
+    @Override
+    public double getSubTotal() {
+        int x = 100;
+        if (this.color.equals("Red") || this.releasedYear == 2025) {
+            x = 120;
+        }
+        return getPrice() * getQuantity() * x / 100;
+    }
+
 }
