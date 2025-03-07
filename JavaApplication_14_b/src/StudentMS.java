796acf244abba60b58eaba501432d541b1b93bb0
Tung Le <lenhattung@gmail.com>
2025-03-07 16:02:54 +0700

    ud
    ud

---
+++ JavaApplication_14_b/src/StudentMS.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tungi
+ */
+public class StudentMS {
+
+    private Student[] list;
+    private int numberOfStudents;
+
+    public StudentMS() {
+        list = new Student[1000];
+        numberOfStudents = 0;
+    }
+
+    public void addStudent(Student st) {
+        //
+    }
+
+    public void removeStudent(Student st) {
+
+    }
+
+    public void printAll() {
+
+    }
+
+    public void printPassedStudent() {
+
+    }
+
+    public boolean contains(Student st) {
+
+    }
+
+    public Student searchByName(String name) {
+
+    }
+}
