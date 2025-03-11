d7cdd94d92e3916cf10e91c21d2d7779656c69ae
Tung Le <lenhattung@gmail.com>
2025-03-11 16:24:46 +0700

    ud
    ud

--- ASM/ASM05/Q5/src/src/VendorList.java
+++ ASM/ASM05/Q5/src/src/VendorList.java
@@ -12,5 +12,37 @@ import java.util.ArrayList;
  * @author tungi
  */
 public class VendorList extends ArrayList<Vendor>{
-    
+    public void addVendor(Vendor v){
+        this.add(v);
+    }
+    public double getVendorSaleById(int id){
+        for (Vendor v : this) {
+            if(v.getId()==id){
+                return v.getSale();
+            }
+        }
+        return Double.parseDouble(String.format("%.3f", id));
+    }
+    public double getTotalSaleAllVendors(){
+        double total = 0;
+        for (Vendor v : this) {
+            total += v.getSale();
+        }
+        return total;
+    }
+    public Vendor getVendorWithMaxSale(){
+        // Tìm max
+        double maxSale = Double.MIN_VALUE;
+        for (Vendor v : this) {
+            if(v.getSale()>maxSale){
+                maxSale = v.getSale();
+            }
+        }
+        // Tìm vendor có giá trị maxSlae
+        for (Vendor v : this) {
+            if(v.getSale()==maxSale)
+                return v;
+        }
+        return null;
+    }
 }
